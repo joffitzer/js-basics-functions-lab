341e83e3a31c3ff4e2d6105c4fac4eadd2470d5e
joffitzer <joffitzer@gmail.com>
2019-12-07 17:42:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    if (blocks > 42) {
+        return blocks - 42;
+    } else {
+        return 42 - blocks;
+    }
+}
+
+function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInBlocks(blocks) * 264;
+}
+
+function distanceTravelledInFeet(start, end) {
+    if (start < end) {
+        return (end - start) * 264;
+    } else {
+        return (start - end) * 264;
+    }
+}
+
+function calculatesFarePrice(start, end) {
+
+    const feet = distanceTravelledInFeet(start, end);
+
+    if (feet < 400) {
+        return 0;
+    } else if (feet > 400 && feet < 2000) {
+        return (feet - 400) * .02;
+    } else if (feet > 2000 && feet < 2500) {
+        return 25;
+    } else {
+        return "cannot travel that far";
+    }
+}
